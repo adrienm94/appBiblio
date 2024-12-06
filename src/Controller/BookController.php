711fd5fd1b85b90e069809257015e22ccdd37330
Adrien Meyrand <adrien.meyrand@gmail.com>
2024-12-06 12:14:28 +0100

    Add book search by title

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use App\Form\BookType;
+use App\Form\SearchBookType;
 use App\Repository\BookRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,8 +14,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class BookController extends AbstractController {
 
-    #[Route("/user/app/books/book-list", name: "app_book_list", methods: ['GET'])]
-    final public function bookList(UserRepository $userRepository, BookRepository $bookRepository){
+    #[Route("/user/app/books/book-list", name: "app_book_list", methods: ['GET', 'POST'])]
+    final public function bookList(UserRepository $userRepository, Request $request, BookRepository $bookRepository){
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -22,13 +23,41 @@ class BookController extends AbstractController {
         $connectedUserIdentifier = $this->getUser()->getUserIdentifier(); // email user
         $connectedUser = $userRepository->findBy(["email" => $connectedUserIdentifier])[0];
 
+        $form = $this->createForm(SearchBookType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $request->request->all()['search_book']["search"];
+            return $this->redirectToRoute("app_book_search", ["search" => $search]);
+        }
+
         $books = $bookRepository->findAll();
 
         return $this->render("book/book-list.html.twig",[
             'books' => $books,
+            'search_form' => $form,
             'user' => $connectedUser
         ]);
 
+    }
+
+    // Méthode de controller pour cherchers un auteur par son prénom
+    #[Route("/user/app/books/book-list/search/{search}", name: 'app_book_search')]
+    final public function searchBookByTitle(BookRepository $bookRepository, UserRepository $userRepository, string $search){
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // on obtient les infos de l'utilisateur connecté
+        $connectedUserIdentifier = $this->getUser()->getUserIdentifier(); // email user
+        $connectedUser = $userRepository->findBy(["email" => $connectedUserIdentifier])[0];
+
+        $booksFound = $bookRepository->findBy(["title" => $search]);
+        return $this->render("book/book-list.html.twig",[
+            'books' => $booksFound,
+            'search_form' => '',
+            'user' => $connectedUser
+        ]);
     }
 
     #[Route("/admin/app/books/add-book", name:"app_books_add_book", methods: ['GET', 'POST'])]
