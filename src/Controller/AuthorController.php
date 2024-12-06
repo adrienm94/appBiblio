53d0c7717ea22eea400e7616533e43c01c931042
Adrien Meyrand <adrien.meyrand@gmail.com>
2024-12-06 11:56:39 +0100

    Add author search by first name

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Author;
 use App\Form\AuthorType;
+use App\Form\SearchAuthorType;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,21 +13,44 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AuthorController extends AbstractController
 {
-    #[Route("/user/app/authors/author-list", name: "app_author_list", methods: ['GET'])]
-    final public function bookList(AuthorRepository $authorRepository){
+    #[Route("/user/app/authors/author-list", name: "app_author_list", methods: ['GET', 'POST'])]
+    final public function authorList(Request $request, AuthorRepository $authorRepository){
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $form = $this->createForm(SearchAuthorType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $request->request->all()['search_author']["search"];
+            return $this->redirectToRoute("app_author_search", ["search" => $search]);
+        }
 
         $authors = $authorRepository->findAll();
 
         return $this->render("author/author-list.html.twig",[
-            'authors' => $authors
+            'authors' => $authors,
+            'search_form' => $form
         ]);
 
     }
 
+    // Méthode de controller pour cherchers un auteur par son prénom
+    #[Route("/user/app/authors/author-list/search/{search}", name: 'app_author_search')]
+    final public function searchAuthorByFirstName(AuthorRepository $authorRepository, string $search){
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $authorsFound = $authorRepository->findBy(["firstName" => $search]);
+        return $this->render("author/author-list.html.twig",[
+            'authors' => $authorsFound,
+            'search_form' => ''
+        ]);
+    }
+
     #[Route("/admin/app/authors/add-author", name:"app_authors_add_author", methods: ['GET', 'POST'])]
-    final public function addBook(Request $request, EntityManagerInterface $entityManager){
+    final public function addAuthor(Request $request, EntityManagerInterface $entityManager){
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -48,7 +72,7 @@ class AuthorController extends AbstractController
     }
 
     #[Route("/admin/app/authors/edit-author/{id<\d+>}", name: "app_authors_edit_author", methods: ['GET', 'POST'])]
-    final public function editBook(int $id, AuthorRepository $authorRepository, Request $request, EntityManagerInterface $entityManager){
+    final public function editAuthor(int $id, AuthorRepository $authorRepository, Request $request, EntityManagerInterface $entityManager){
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -69,7 +93,7 @@ class AuthorController extends AbstractController
     }
 
     #[Route("/admin/app/authors/delete-author/{id<\d+>}", name: "app_authors_delete_author", methods: ['GET', 'POST'])]
-    final public function deleteBook(int $id, AuthorRepository $authorRepository, EntityManagerInterface $entityManager){
+    final public function deleteAuthor(int $id, AuthorRepository $authorRepository, EntityManagerInterface $entityManager){
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
